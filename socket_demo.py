14f7a92dbf2a127eec792033ff88b40da049ef84
David Banks <amoe@solasistim.net>
2023-03-07 19:22:09 +0000

    adding whisper task

--- socket_demo.py
+++ socket_demo.py
@@ -5,7 +5,10 @@ import torch
 import multiprocessing
 
 
-def task(queue, x, y):
+def task(queue, input_path):
+    output_dir = '/srv/cifs_rw/whisper_transcriptions'
+    unique_filename = str(uuid.uuid4()) + '.srt'
+    output_path = os.path.join(output_dir, unique_filename)
     cuda = torch.cuda.is_available()
 
     if cuda:
@@ -13,14 +16,30 @@ def task(queue, x, y):
     else:
         print("CUDA not available.")
 
-
     print("Loading model.")
-    model = stable_whisper.load_model('medium')
+    model = stable_whisper.load_model('small')   # loopkever can only use small
     print("Loaded model.")
 
-    time.sleep(5)
-    queue.put(x + y)
+    no_speech_threshold = 0.7
+    compression_ratio_threshold = 1.7
+    beam_size = 4
+    best_of = 3
+    temperature = 0.0
 
+    results = model.transcribe(
+        input_path, language='fr', verbose=False,
+        condition_on_previous_text=False,
+        no_speech_threshold=no_speech_threshold,
+        compression_ratio_threshold=compression_ratio_threshold,
+        beam_size=beam_size,
+        best_of=best_of,
+        temperature=temperature
+    )
+
+    stable_whisper.results_to_sentence_srt(results, output_path)
+    queue.put({'success': True})
+
+    
 SOCKET_BACKLOG = 0
 BIND_HOST = '192.168.0.1'
 
@@ -50,7 +69,7 @@ while True:
     command = parts[0]
 
     if command == 's':
-        process = multiprocessing.Process(target=task, args=(queue, 1,2))
+        process = multiprocessing.Process(target=task, args=(queue, parts[1]))
         process.start()
         print("Created whisper process with pid", process.pid)
     elif command == 'q':
