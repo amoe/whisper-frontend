94070d5357cad317a283fe0487114975b560e6fb
David Banks <amoe@solasistim.net>
2023-03-21 08:10:54 +0000

    submit multiplejobs at once

--- client.py
+++ client.py
@@ -1,4 +1,11 @@
 from xmlrpc.client import ServerProxy
+import sys
 
-with ServerProxy('http://localhost:49153/') as proxy:
-    print(proxy.start_job('test.mkv'))
+with ServerProxy('http://elang:49152/') as proxy:
+    with open('/home/amoe/rikerpaths') as f:
+        for line in f:
+            name = line.rstrip()
+            proxy.start_job(name)
+
+
+
