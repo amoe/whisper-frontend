45f224bd18898f277ebad3e12e38c660bf334373
David Banks <amoe@solasistim.net>
2024-08-31 10:28:39 +0100

    updating client configuration

--- client2.py
+++ client2.py
@@ -1,7 +1,10 @@
 from xmlrpc.client import ServerProxy
 import sys
 
-with ServerProxy('http://elang:49152/') as proxy:
+REMOTE_HOST = 'localhost'
+REMOTE_PORT = 49152
+
+with ServerProxy(f'http://{REMOTE_HOST}:{REMOTE_PORT}/') as proxy:
     proxy.start_job(sys.argv[1], sys.argv[2])
 
 
