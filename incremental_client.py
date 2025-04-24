e7e430b83495a0396689dd88b93fb51b7dfe2955
David Banks <amoe@solasistim.net>
2025-04-24 07:23:45 +0100

    language option for incremental client

--- incremental_client.py
+++ incremental_client.py
@@ -8,6 +8,8 @@ Send jobs for everything not yet analysed in a given directory.
 
 Provide a filename containing a list of paths (in a server-compatible format)
 on the command line.
+
+Also provide a language code e.g. 'en', 'fr'.
 """
 
 def fetch_transcribed(config):
@@ -38,17 +40,19 @@ with open('whisper-frontend.ini') as f:
 transcribed = set(fetch_transcribed(config))
 
 path_file = sys.argv[1]
+language_code = sys.argv[2]
 
 with open(path_file) as f:
     lines = set([l.rstrip() for l in f.readlines()])
 
 
 diff = lines.difference(transcribed)
+
 print(len(transcribed))
 print(len(lines))
-print(len(diff))
+print("I will submit", len(diff), "files to the job server.")
 
 
 with ServerProxy('http://elang:49152/') as proxy:
     for path in diff:
-        proxy.start_job(path, 'fr')
+        proxy.start_job(path, language_code)
